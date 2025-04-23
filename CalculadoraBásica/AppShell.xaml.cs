b1495e46cc27eeb50ce8156c8abbf55b8cbf32a0
Aalvebia <aalvebia03@gmail.com>
2025-04-23 10:43:25 -0300

    Add project files.

---
+++ CalculadoraBásica/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraBásica
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CalculadoraBásica/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace CalculadoraBásica;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCalcularCliked(object sender, EventArgs e)
+    {
+        if(!double.TryParse(Num1Entry.Text, out double numero1) ||
+           !double.TryParse(Num2Entry.Text, out double numero2))
+        {
+            ResultadoLabel.Text = "Por favor, insira números válidos! ";
+            return;
+        }
+        
+        if(OperacaoEscolhida.SelectedIndex == -1)
+        {
+            ResultadoLabel.Text = "Selecione uma operação.";
+            return;
+        }
+
+        String operacao = OperacaoEscolhida.SelectedItem.ToString();
+        double resultado = 0;
+
+        switch (operacao)
+        {
+            case "Somar":
+                resultado = numero1 + numero2;
+                break;
+
+            case "Subtrair":
+                resultado = numero1 - numero2;
+                break;
+
+            case "Multiplicar":
+                resultado = numero1 * numero2;
+                break;
+
+            case "Dividir":
+                if(numero2 == 0)
+                {
+                    ResultadoLabel.Text = "Não é possivel dividir por zero.";
+                    return;
+                }
+               
+                    resultado = numero1 / numero2;
+                    break;
+        }
+        ResultadoLabel.Text = $"Resultado: {resultado}";
+    }
+}
+
+
